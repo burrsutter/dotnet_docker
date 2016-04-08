765def275d3a7863cdf158e02a205c8bbf38b064
Burr Sutter <bsutter@redhat.com>
2016-04-08 14:29:03 -0400

    adding helloweb

---
+++ helloweb/README.md
@@ -0,0 +1,6 @@
+docker build -t dotnet_helloweb .
+
+docker run -it -p 5000:5000 dotnet_helloweb
+
+
+
---
+++ helloweb/Startup.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+
+
+namespace HelloWeb
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello World! " + System.DateTime.Now);
+            });
+        }
+    }
+}
